b7f3ae79fd37b4bdf2abc63f5306c1d80e6072b5
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 19:13:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employees_count
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+  
+end
