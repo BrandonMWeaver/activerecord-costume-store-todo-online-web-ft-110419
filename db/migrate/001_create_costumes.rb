7f568ebc37c51235a46da6bbd4ddae97e2e01bd6
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 19:16:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,6 +3,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.string :price
+      t.string :image_url
+      t.string :size
     end
   end
   
