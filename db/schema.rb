c411729119aca5285c13eae67f9e29c34545e59c
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 19:19:14 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.string "price"
+    t.string "opening_date"
+    t.string "closing_date"
+    t.string "description"
   end
 
 end
